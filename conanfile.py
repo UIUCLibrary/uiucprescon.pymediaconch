dba4a57cc848eb118b7bae2ab14c8d0522ed1105
hborcher <hborcher@illinois.edu>
2026-06-04 13:30:12 -0500

    bump: use libmediainfo/26.01

--- conanfile.py
+++ conanfile.py
@@ -1,6 +1,5 @@
 from conan import ConanFile
 from conan.tools.cmake import cmake_layout
-# from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout, CMakeDeps
 
 class MediaConch(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
@@ -10,8 +9,7 @@ class MediaConch(ConanFile):
         self.requires("zlib/1.3.1")
         self.requires("libxslt/1.1.43")
         self.requires("libxml2/2.13.8")
-        self.requires("libzen/0.4.38")
-        self.requires("libmediainfo/22.03")
+        self.requires("libmediainfo/26.01")
 
     def layout(self):
         cmake_layout(self)
